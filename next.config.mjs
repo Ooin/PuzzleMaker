e3225412e13cd202210724a5a3bdf845bf670936
Ooin <xaxipistaxi@gmail.com>
2026-05-15 12:51:12 +0200

    Update Next.js configuration for GitHub Actions deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
+const repo = process.env.GITHUB_REPOSITORY?.split("/")[1] || "PuzzleMaker";
+const isActions = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig = {
   output: "export",
+  basePath: isActions ? `/${repo}` : "",
+  assetPrefix: isActions ? `/${repo}/` : "",
+  trailingSlash: true,
 };
 export default nextConfig;
